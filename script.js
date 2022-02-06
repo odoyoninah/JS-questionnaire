2ffe253564021f82fbad96438a66b3b1c33f455b
odoyonina <odoyoninah@gmail.com>
2022-02-06 21:41:43 +0300

    style:add form

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
-let userForm = document.getElementById("userForm");
-let score = 0;
+function submitted(){
+    var score = 0;
 
-userForm.addEventListener("submit",function (event){event.preventDefault})
-var userData = new FormData(userForm);
-compareData(userData);
+    var correctAnswers = {
+        datatype:"letters",
+        modulus:"percentage",
+        equals:"Strictly equality",
+        rules:"Must have quotation marks",
+        functions:"Anonymous function",}
 
-if (datatype === correctAnswers["letters"]) {
-    score +=4;
-} else { alert("not correct");}
 
-
-var correctAnswers = {
-    datatype:"letters",
-    modulus:"percentage",
-    equals:"Strictly equality",
-    rules:"Must have quotation marks",
-    functions:"Anonymous function",
-}
+var userForm = document.getElementById("userForm");
 
 
 
-function compareData(userData){
-    var datatype = userData.get("letters");
-    var modulus = userData.get("percentage");
-
-var equals = userData.get("Strictly equality");
-var rules = userData.get("Must have quotation marks");
-var functions = userData.get("Anonymous function");}
 
 
+// var userData = new FormData(userForm);
+// compareData(userData);
 
-let userScore = document.querySelector(".score");
-userScore.textContent = score;
-userScore.getElementsByClassName.color = "red";
-userForm .style.display = "none";
+// if (datatype === correctAnswers["letters"]) {
+//     score +=4;
+// } else { alert("not correct");}
+
+
+
+// }
+
+
+
+// function compareData(userData){
+//     var datatype = userData.get("letters");
+//     var modulus = userData.get("percentage");
+
+// var equals = userData.get("Strictly equality");
+// var rules = userData.get("Must have quotation marks");
+// var functions = userData.get("Anonymous function");}
+
+
+
+// let userScore = document.querySelector(".score");
+// userScore.textContent = score;
+// userScore.getElementsByClassName.color = "red";
+// userForm .style.display = "none";
